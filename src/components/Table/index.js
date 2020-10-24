3ae477adf8db26e6637443adcdd5ed5340dcdb78
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-10-24 15:41:48 -0400

    successfully rendering employee list now

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,19 +1,50 @@
 import React from "react";
 
 function DataTable({ data }) {
-    const columns = data[0] && Object.keys(data[0]);
+    console.log(data)
+    const columns = [
+        {
+            name: "name",
+            displayName: "Employee Name"
+        },
+        {
+            name: "dob",
+            displayName: "Date of Birth"
+        },
+        {
+            name: "email",
+            displayName: "Email"
+        },
+        {
+            name: "phone",
+            displayName: "Phone Number"
+        }
+    ];
+
+    function placeColumn (colName, colObj, idx) {
+        console.log(colObj);
+        switch (colName) {
+            case "name":
+                return <td key={idx}>{`${colObj[colName].first} ${colObj[colName].last}`}</td>
+            case "phone":
+            case "email":
+                return <td key={idx}>{colObj[colName]}</td>
+            case "dob": 
+                return <td key={idx}>{colObj[colName].date}</td>
+            default: 
+                return <td></td>
+        }
+    }
     return (
-        <table cellPadding={0} cellSpacing={0}>
+        <table cellPadding={4} cellSpacing={4}>
             <thead>
-                <tr>{data[0] && columns.map(heading => <th>{heading}</th>)}</tr>
+                <tr>{data[0] && columns.map((col, idx) => <th key={idx}>{col.displayName}</th>)}</tr>
             </thead>
             <tbody>
-                {data.map(row => (
-                <tr> 
-                    {columns.map(column => (
-                    <td>{row[column]}</td>
-                    ))}
-                </tr>
+                {data[0].map((row, idx) => (
+                    <tr key={idx}> 
+                        {columns.map((column, idx) => placeColumn(column.name, row, idx))}
+                    </tr>
                 ))}
             </tbody>
         </table>
