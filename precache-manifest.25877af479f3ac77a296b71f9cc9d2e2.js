06085c94a1e5fc34d74d6c8687249ba748d1d651
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-10-27 18:58:14 -0400

    Updates

---
+++ precache-manifest.25877af479f3ac77a296b71f9cc9d2e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4a7a9f55fc72794640dbb228e0aa04",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "c2fba7baea015d890e2e",
+    "url": "/React-Employee-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e7d599e6cb3bec18731e",
+    "url": "/React-Employee-Directory/static/css/main.49bc2399.chunk.css"
+  },
+  {
+    "revision": "c2fba7baea015d890e2e",
+    "url": "/React-Employee-Directory/static/js/2.62ddcfd7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Employee-Directory/static/js/2.62ddcfd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d599e6cb3bec18731e",
+    "url": "/React-Employee-Directory/static/js/main.e21bbbab.chunk.js"
+  },
+  {
+    "revision": "e0088999937cbd81d387",
+    "url": "/React-Employee-Directory/static/js/runtime-main.c748d7d3.js"
+  }
+]);
--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
-export default {
-  getEmployeeList: function() {
-    return axios.get("https://randomuser.me/api/?results=100&inc=name,phone,email,dob,picture&nat=us&noinfo")
-  }
-};
